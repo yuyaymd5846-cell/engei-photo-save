2a4a1317aa71d7ac17777572c04c27a4f0785820
yuyaymd5846-cell <yuya.ymd5846@gmail.com>
2026-04-19 12:02:26 +0900

    SWキャッシュバスト(v3) + エラー詳細表示

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'photo-save-v1';
+const CACHE = 'photo-save-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
